7481e950531bd9d4d07f1099441b6203e12091d1
William Shiao <willshiao@gmail.com>
2017-01-21 15:01:55 -0800

    add favicon

--- api/app.js
+++ api/app.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const config = require('config');
+const favicon = require('serve-favicon');
+const path = require('path');
 const app = express();
 
 const db = require('./lib/db');
@@ -9,6 +11,7 @@ const db = require('./lib/db');
 const indexRoutes = require('./routes/index');
 const pageRoutes = require('./routes/pages');
 
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 app.use('/', indexRoutes);
 app.use('/pages', pageRoutes);
 app.use(express.static('public'));
