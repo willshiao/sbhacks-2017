60205afba53069d9100f624bbda49a599d9c0815
William Shiao <willshiao@gmail.com>
2017-01-20 22:47:28 -0800

    add bulk add page

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,6 +3,8 @@
 const router = require('express').Router();
 const Seller = require('../models/Seller');
 const bodyParser = require('body-parser');
+const Promise = require('bluebird');
+const _ = require('lodash');
 
 router.use(bodyParser.urlencoded({extended: true}));
 
@@ -25,7 +27,25 @@ router.post('/sellers', (req, res) => {
     })
     .catch(err => {
       return res.json({success: false, error: err});
+    });
+});
+
+router.post('/sellers/bulk', (req, res) => {
+  Promise.all(_.chunk(req.body.bulk.split(/\r?\n/), 3)
+    .map(c => {
+      if(c.length < 3) return Promise.resolve();
+      console.log('c:', c);
+      return new Seller({
+        info: {Name: c[0]},
+        coords: [c[1], c[2]]
+      }).save();
+    }))
+    .then(() => {
+      return res.json({success: true});
     })
+    .catch(err => {
+      return res.json({success: false, error: err});
+    });
 });
 
 module.exports = router;
